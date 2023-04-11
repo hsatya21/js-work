315ba75151364ee28efef4331d0f15d8cfd57198
Harshit Satya <hsatya@systango.com>
2023-04-11 11:35:31 +0530

    dom properties

---
+++ 06-dom-intro/script.js
@@ -0,0 +1,13 @@
+// console.dir(document);
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hello World</h1>';
+
+// document.write('Hello from JS');
+
+console.log(document.getElementById('main'));
+// const main = document.getElementById('main');
+// main.innerHTML = '<p>Hello from main</p>';
+
+console.log(document.querySelector('#main h1'));
+document.querySelector('#main h1').innerText = 'Hello';
